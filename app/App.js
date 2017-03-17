4a0a09e8cade16accd77396ca65a303e35eb689e
Erik Davtyan <erikdavtyan@gmail.com>
2017-03-17 21:38:54 +0400

    final commit

--- app/App.js
+++ app/App.js
@@ -7,35 +7,32 @@ class App extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      search: "",
-      countries: Countries,
+      search: ""
     }
   }
 
   changeSearch = (e) => {
-    const search = e.currentTarget.value;
+    const search = e.currentTarget.value.toLowerCase();
     this.setState({ search });
   }
 
   render() {
 
-    const renderCountries = this.state.countries.map((currentCountry, idx) => {
-      if (currentCountry.name.toLowerCase().includes(this.state.search.toLowerCase())
-        ||
-        currentCountry.code.toLowerCase().includes(this.state.search.toLowerCase())) {
-        return <li key={idx}>{currentCountry.name}</li>
+    const renderCountries = Countries.map((country, idx) => {
+      const {name, code} = country;
+      const {search} = this.state;
+      if (name.toLowerCase().includes(search) || code.toLowerCase().includes(search)) {
+        return <div className="country-element" key={idx}>{name}</div>;
       }
     });
 
     return(
       <div className="main-container">
-        <div className="container">
-          <h1>Filtered list</h1>
+        <div className="container title">
+          Filtered list
         </div>
-        <div className="container">
-          <input placeholder="Filter..." onChange={this.changeSearch} />
-        </div>
-        <div className="container">
+        <input className="container search-field" placeholder="Filter..." onChange={this.changeSearch} />
+        <div className="container list">
           {renderCountries}
         </div>
       </div>
